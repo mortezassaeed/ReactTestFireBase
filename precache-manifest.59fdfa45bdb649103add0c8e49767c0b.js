7197e267ba51b3db997a6ee3e947ef357211ebd1
Morteza Salehi <mortezassaeed@gmail.com>
2020-05-22 17:08:09 +0430

    Updates

---
+++ precache-manifest.59fdfa45bdb649103add0c8e49767c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d22a32e13ffdf1c9ba004b94d46c62f",
+    "url": "/ReactTestFireBase/index.html"
+  },
+  {
+    "revision": "2c7b2896be185fbc09c2",
+    "url": "/ReactTestFireBase/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52d632ef489aaf7177b9",
+    "url": "/ReactTestFireBase/static/js/2.e75b4556.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/ReactTestFireBase/static/js/2.e75b4556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7b2896be185fbc09c2",
+    "url": "/ReactTestFireBase/static/js/main.f747db3f.chunk.js"
+  },
+  {
+    "revision": "2deac67134026174a770",
+    "url": "/ReactTestFireBase/static/js/runtime-main.ca24a198.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTestFireBase/static/media/logo.5d5d9eef.svg"
+  }
+]);
